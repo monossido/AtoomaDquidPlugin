fa9cd0dd87368d962ee81442b7212484a2759322
tres <nicolo.tresoldi@hotmail.it>
2014-05-17 22:08:30 +0200

    Completata tts

--- src/com/atooma/plugin/plugindquid/SpeechActivity.java
+++ src/com/atooma/plugin/plugindquid/SpeechActivity.java
@@ -17,14 +17,15 @@ import android.speech.tts.UtteranceProgressListener;
 import android.util.Log;
 
 public class SpeechActivity extends Activity implements OnInitListener, OnUtteranceCompletedListener {
-
+	private Intent dataFromActivity;
+	
 	private int VOICE_RECOGNITION_REQUEST_CODE = 12345;
 	private TextToSpeech tts;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		tts = new TextToSpeech(this, this);
+		
 
 	}
 
@@ -32,36 +33,11 @@ public class SpeechActivity extends Activity implements OnInitListener, OnUttera
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
 		if (requestCode == VOICE_RECOGNITION_REQUEST_CODE && resultCode == RESULT_OK) {
-
-			ArrayList matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-
+			dataFromActivity = data;
+			tts = new TextToSpeech(this, this);
 			// TODO
 
-			for (Object match : matches) {
-				Log.v("DQUIDPLUGIN", "match=" + match);
-				String stringMatch = (String) match;
-				LocationManager locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
-				if (stringMatch.equalsIgnoreCase("sì")) {
-					String latitudineArrivo = getIntent().getExtras().getString("lat");
-					String longitudineArrivo = getIntent().getExtras().getString("lon");
-					Location posizione = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-
-					double latitudineDouble = Double.parseDouble(latitudineArrivo);
-					latitudineDouble += 0.1;
-
-					double longitudineDouble = Double.parseDouble(longitudineArrivo);
-					longitudineDouble += 0.1;
-
-					String url = "http://maps.google.com/maps?saddr=" +
-							latitudineDouble + "," +
-							longitudineDouble + "&daddr=" +
-							latitudineArrivo + "," + longitudineArrivo;
-					Intent navigatore = new Intent(Intent.ACTION_VIEW);
-					navigatore.setData(Uri.parse(url));
-					startActivity(navigatore);
-				}
-
-			}
+			
 		}
 
 		super.onActivityResult(requestCode, resultCode, data);
@@ -80,7 +56,33 @@ public class SpeechActivity extends Activity implements OnInitListener, OnUttera
 	}
 	
 	public void onUtteranceCompleted(String utteranceId) {
-        //do something, e.g. update the GUI or something like that (beware of threading)
+		if(dataFromActivity!=null){
+			ArrayList matches = dataFromActivity.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+			for (Object match : matches) {
+				Log.v("DQUIDPLUGIN", "match=" + match);
+				String stringMatch = (String) match;
+				LocationManager locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+				if (stringMatch.equalsIgnoreCase("sì")) {
+					String latitudineArrivo = getIntent().getExtras().getString("lat");
+					String longitudineArrivo = getIntent().getExtras().getString("lon");
+					Location posizione = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+	
+					double latitudineDouble = Double.parseDouble(latitudineArrivo);
+					latitudineDouble += 0.1;
+	
+					double longitudineDouble = Double.parseDouble(longitudineArrivo);
+					longitudineDouble += 0.1;
+	
+					String url = "http://maps.google.com/maps?saddr=" +
+							latitudineDouble + "," +
+							longitudineDouble + "&daddr=" +
+							latitudineArrivo + "," + longitudineArrivo;
+					Intent navigatore = new Intent(Intent.ACTION_VIEW);
+					navigatore.setData(Uri.parse(url));
+					startActivity(navigatore);
+				}
+			}
+		}
     }
 
 }
