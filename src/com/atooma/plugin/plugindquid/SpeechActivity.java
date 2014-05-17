baa3911d952bbe48dde1af3d3a702598566ee52f
Lorenzo Braghetto <monossido@lorenzobraghetto.com>
2014-05-18 00:52:11 +0200

    fixxxxxxxxxxxx

--- src/com/atooma/plugin/plugindquid/SpeechActivity.java
+++ src/com/atooma/plugin/plugindquid/SpeechActivity.java
@@ -14,11 +14,10 @@ import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.TextToSpeech.OnInitListener;
 import android.speech.tts.TextToSpeech.OnUtteranceCompletedListener;
-import android.speech.tts.UtteranceProgressListener;
 import android.util.Log;
 
 public class SpeechActivity extends Activity implements OnInitListener, OnUtteranceCompletedListener {
-	
+
 	private int VOICE_RECOGNITION_REQUEST_CODE = 12345;
 	private TextToSpeech tts;
 
@@ -31,39 +30,39 @@ public class SpeechActivity extends Activity implements OnInitListener, OnUttera
 
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		
-		
+
 		if (requestCode == VOICE_RECOGNITION_REQUEST_CODE && resultCode == RESULT_OK) {
 			ArrayList matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
 			for (Object match : matches) {
 				Log.v("DQUIDPLUGIN", "match=" + match);
 				String stringMatch = (String) match;
 				LocationManager locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+				Log.v("DQUIDPLUGIN", "stringMatch=" + stringMatch);
 				if (stringMatch.equalsIgnoreCase("sì")) {
 					String latitudineArrivo = getIntent().getExtras().getString("lat");
 					String longitudineArrivo = getIntent().getExtras().getString("lon");
 					Location posizione = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-	
+
 					double latitudineDouble = Double.parseDouble(latitudineArrivo);
 					latitudineDouble += 0.1;
-	
+
 					double longitudineDouble = Double.parseDouble(longitudineArrivo);
 					longitudineDouble += 0.1;
-	
+
 					String url = "http://maps.google.com/maps?saddr=" +
 							latitudineDouble + "," +
 							longitudineDouble + "&daddr=" +
-							
+
 							latitudineArrivo + "," + longitudineArrivo;
 					Intent navigatore = new Intent(Intent.ACTION_VIEW);
-					
+
 					navigatore.setData(Uri.parse(url));
-					
+
 					startActivity(navigatore);
 
-			
+				}
+			}
 		}
-			}}
 		super.onActivityResult(requestCode, resultCode, data);
 	}
 
@@ -72,23 +71,22 @@ public class SpeechActivity extends Activity implements OnInitListener, OnUttera
 		tts.setLanguage(Locale.ITALY);
 		HashMap<String, String> params = new HashMap<String, String>();
 
-		params.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID,"stringId");
+		params.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "stringId");
 		tts.speak("Benzina sotto il 20 per cento, vuoi essere guidato al distributore più vicino?", TextToSpeech.QUEUE_ADD, params);
-		
+
 		tts.setOnUtteranceCompletedListener(this);
-		
+
 	}
-	
+
 	public void onUtteranceCompleted(String utteranceId) {
 		Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
 
 		intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Select an application"); // user hint
 		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH); // setting recognition model, optimized for short phrases – search queries
 		intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 1); // quantity of results we want to receive
-		Log.v("lancio","lancio");
-		startActivity(intent);
-	
-	
-    }
+		Log.v("lancio", "lancio");
+		startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
+
+	}
 
 }
